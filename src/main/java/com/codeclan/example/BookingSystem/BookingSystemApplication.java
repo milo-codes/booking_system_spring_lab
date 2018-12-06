23a51c0bcc597a8cc19d34986a138098151e1086
Genna Walsh <gennawalsh@gmail.com>
2018-12-06 11:24:24 +0000

    first commit

---
+++ src/main/java/com/codeclan/example/BookingSystem/BookingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.codeclan.example.BookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemApplication.class, args);
+	}
+}
---
+++ src/main/java/com/codeclan/example/BookingSystem/models/Booking.java
@@ -0,0 +1,4 @@
+package com.codeclan.example.BookingSystem.models;
+
+public class Booking {
+}
